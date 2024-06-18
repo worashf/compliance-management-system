8bca8817ba4831eb0dd33bd92764edf898e4a1e8
worashf <uppert83@gmail.com>
2024-06-18 22:44:00 +0300

    Remove Validation

--- backend/models/Compliant.js
+++ backend/models/Compliant.js
@@ -2,16 +2,13 @@ const mongoose  = require("mongoose")
 
 const compliantSchema  =  mongoose.Schema({
    compliantTitle: {type: String, required: true,  
-       minlength: [50, 'compliant title is too short'],
-       maxlength: [100, 'compliant title is too long'],
   },
    compliantDescription: {type: String,
     required: true,
-    minlength: [100, 'Description is too short'],maxlength: [1000, 'Description is too long'],},
+   },
     compliantCategory: { type: String, required: true},
     compliantEventDate: {type:  Date,  required: true},
     wantToBeDone: {type:  String, required: true,
-      minlength: [100, 'what you want to be done is too short'],maxlength: [1000, 'what you want to be done is too long'],
     },
     compliantSourceInstitution: {    type: mongoose.Schema.Types.ObjectId,
       ref: "Institution",
